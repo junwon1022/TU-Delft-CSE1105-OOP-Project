80831558afd55c266b896ac33d8f484e01b49a9a
junwon <J.Yoon@student.tudelft.nl>
2023-03-05 14:45:03 +0100

    Fixed checkstyle in Server folder

--- server/src/main/java/server/Config.java
+++ server/src/main/java/server/Config.java
@@ -23,6 +23,11 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class Config {
 
+    /**
+     * Create a random number generator.
+     *
+     * @return
+     */
     @Bean
     public Random getRandom() {
         return new Random();
