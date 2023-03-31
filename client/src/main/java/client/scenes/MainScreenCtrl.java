53f999f6bf2ecd850b693a612895612f4d0bf920
asavova03 <A.Savova@student.tudelft.nl>
2023-03-31 18:47:08 +0200

    Added colors to admin screen

--- client/src/main/java/client/scenes/MainScreenCtrl.java
+++ client/src/main/java/client/scenes/MainScreenCtrl.java
@@ -165,7 +165,7 @@ public class MainScreenCtrl {
                 String password = controller.password;
                 String backgroundColor = controller.backgroundColor;
                 String fontColor = controller.fontColor;
-                System.out.println("The title is "+ title);
+                System.out.println("The title is " + title);
 
                 Board board = new Board(title, backgroundColor, fontColor,"#b6e9f3" ,
                         "" , password, new ArrayList<>(), new HashSet<>(), null);
