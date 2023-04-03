e52c0a1294df3d03b285b57e9efd09262b17f229
junwon <J.Yoon@student.tudelft.nl>
2023-04-03 23:57:01 +0200

    HelpScreen available from all Screen

--- client/src/main/java/client/scenes/MainScreenCtrl.java
+++ client/src/main/java/client/scenes/MainScreenCtrl.java
@@ -199,6 +199,27 @@ public class MainScreenCtrl {
     public void disconnect(ActionEvent event) {
         mainCtrl.showConnect();
     }
+
+    /**
+     * Opens the help screen
+     * @param event - Key event when the user presses shift + /
+     */
+    public void openHelpScreen(KeyEvent event) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("HelpScreen.fxml"));
+
+        if(event.getCode().toString().equals("SLASH") && event.isShiftDown()) {
+            try {
+                Parent root = fxmlLoader.load();
+
+                Stage stage = new Stage();
+                stage.setTitle("Help");
+                stage.setScene(new Scene(root, 600, 400));
+                stage.showAndWait();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
 }
 
 
