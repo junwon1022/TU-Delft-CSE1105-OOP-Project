c3a6c482958120fd8bce1f9caa1dca5cc3cf945c
Luis Cabo Villagomez <L.CaboVillagomez@student.tudelft.nl>
2023-04-04 00:06:25 +0200

    Fix constructor error

--- client/src/main/java/client/scenes/CardDetailsCtrl.java
+++ client/src/main/java/client/scenes/CardDetailsCtrl.java
@@ -212,7 +212,6 @@ public class CardDetailsCtrl{
     private CheckListItem createCheckList(String description) {
         CheckListItem checkListItem = new CheckListItem(
                 description,
-                data.size(),
                 false,
                 card);
         return checkListItem;
